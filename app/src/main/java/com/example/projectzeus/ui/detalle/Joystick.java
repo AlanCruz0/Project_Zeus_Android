f63f7acf534ddd2c54cb72a3cd536eec1619dbda
Alan <21170195@uttcampus.edu.mx>
2024-04-12 04:49:46 -0600

    Modulo bluetooth configurado correctamente

--- app/src/main/java/com/example/projectzeus/ui/detalle/Joystick.java
+++ app/src/main/java/com/example/projectzeus/ui/detalle/Joystick.java
@@ -1,34 +1,28 @@
 package com.example.projectzeus.ui.detalle;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 // Bluetooth
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.os.Handler;
-import android.os.Message;
 import android.widget.Toast;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
 
 import com.example.projectzeus.R;
+import com.example.projectzeus.ui.Detalles_activity;
 
 public class Joystick extends AppCompatActivity {
     private Button btndetener, btnavanzar, btnretroceder;
@@ -47,38 +41,24 @@ public class Joystick extends AppCompatActivity {
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        if (!checkBluetoothPermission()) {
-            requestBluetoothPermission();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "El dispositivo no admite Bluetooth", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, Detalles_activity.class);
+            startActivity(intent);
+            finish();
         }
 
-        @SuppressLint("HandlerLeak") final Handler handler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                super.handleMessage(msg);
-            }
-        };
-
         btndetener.setOnClickListener(v -> {
-            if (checkBluetoothPermission()) {
-                enviarDatosBluetooth("D");
-            }
+            enviarDatosBluetooth("D");
         });
 
         btnavanzar.setOnClickListener(v -> {
-            if (checkBluetoothPermission()) {
-                enviarDatosBluetooth("A");
-            }
+            enviarDatosBluetooth("A");
         });
 
         btnretroceder.setOnClickListener(v -> {
-            if (checkBluetoothPermission()) {
-                enviarDatosBluetooth("R");
-            }
+            enviarDatosBluetooth("R");
         });
-    }
-
-    private boolean checkBluetoothPermission() {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED;
     }
 
     private void requestBluetoothPermission() {
@@ -97,51 +77,58 @@ public class Joystick extends AppCompatActivity {
         }
     }
 
-    private void enviarDatosBluetooth(String data) {
-        if (bluetoothAdapter == null) {
-            Toast.makeText(this, "El dispositivo no admite Bluetooth", Toast.LENGTH_SHORT).show();
-            return;
+    private void closeBluetoothSocket() {
+        if (outputStream != null) {
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                Log.i("Bluetooth", "Error al cerrar el OutputStream");
+            }
+            outputStream = null;
         }
 
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                Log.i("Bluetooth", "Error al cerrar el socket");
+            }
+            socket = null;
+        }
+    }
+
+    private void enviarDatosBluetooth(String data) {
         if (!bluetoothAdapter.isEnabled()) {
             Toast.makeText(this, "Por favor, habilite Bluetooth", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        BluetoothDevice device = bluetoothAdapter.getRemoteDevice("Dirección MAC del dispositivo Bluetooth");
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice("00:23:05:00:3B:6C");
 
         try {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED) {
-                socket = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
-                socket.connect();
-                outputStream = socket.getOutputStream();
+                if (socket == null || !socket.isConnected()) {
+                    socket = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+                    socket.connect();
+                    outputStream = socket.getOutputStream();
+                }
                 outputStream.write(data.getBytes());
-                socket.close();
+                Log.d("Bluetooth", "Datos enviados: " + data);
             } else {
                 requestBluetoothPermission();
             }
+        } catch (SecurityException se) {
+            Toast.makeText(this, "Permiso de Bluetooth denegado", Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
+            closeBluetoothSocket();
             Toast.makeText(this, "Error al enviar datos a través de Bluetooth", Toast.LENGTH_SHORT).show();
-            try {
-                socket = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
-                socket.connect();
-                outputStream = socket.getOutputStream();
-                outputStream.write(data.getBytes());
-            } catch (IOException ex) {
-                ex.printStackTrace();
-                Toast.makeText(this, "Error al reconectar el socket Bluetooth", Toast.LENGTH_SHORT).show();
-            }
-        } finally {
-            try {
-                if (outputStream != null) {
-                    outputStream.close();
-                }
-                if (socket != null) {
-                    socket.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        enviarDatosBluetooth("S");
+        super.onDestroy();
+        closeBluetoothSocket();
     }
 }
